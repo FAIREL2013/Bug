59751d853ad9405990656b88f5c5cdcf5f6474c8
FAIREL2013 <fairelramadhan1234567890@gmail.com>
2026-02-28 05:39:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8784559055:AAF_cOKlnM767Vp2mHdV311dVkJLrdlXR9I",  // Masukin Bot token kamu
+  owners: "8144496840", // Masukin ID Telegram kamu
+  port: "9067", // Masukin Port panel kamu 
+  ipvps: "http://panel.astavvip-tech.xyz/server/9e8f446b" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1995-1996 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
